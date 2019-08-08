924243d4f0c23508d8c734c8d2639e79a3017b5a
Ankita Mhatre <53672776+anki009@users.noreply.github.com>
2019-08-08 20:39:29 +0530

    solution to trip problem using c

---
+++ trip1.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+double  exchange (int n, double expenses[]);
+double  mean( int n, double numbers[]);
+int main(){
+
+int numberOfStudents;
+
+while( scanf("%d", &numberOfStudents) != EOF){
+
+if(numberOfStudents == 0 ){
+
+break;
+}
+
+double expenses[numberOfStudents];
+
+for(int i = 0; i < numberOfStudents; i++){
+   scanf("%lf", &expenses[i]);
+}
+
+double result = exchange( numberOfStudents, expenses );
+result = (double)((int)(result*100.0)) / 100.0;
+printf("$%.2lf\n",result);
+
+}
+
+}
+
+
+double  exchange (int n, double expenses[]){
+
+double ans, avg, result;
+
+avg = mean(n, expenses);
+
+for(int i =0; i < n; i++){
+if(expenses[i] < avg){
+ans = avg - expenses[i];
+result += ans;
+
+}
+
+}
+return result;
+} 
+
+double  mean( int n, double numbers[]){
+double avg;
+ 
+    for(int i = 0; i < n; i++) {
+        avg += numbers[i];
+
+    }
+    return avg / (double)n;
+
+}
+
+/*
+OUTPUT=
+3
+10
+20
+30
+$10.00
+4
+15
+15.01
+3.01
+3
+$11.99
+0
+
+*/
